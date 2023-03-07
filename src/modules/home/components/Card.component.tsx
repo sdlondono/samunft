55db42cd41ddb1384ead18da48727d57ab388af9
ConDish <samioff15@gmail.com>
2023-03-07 00:12:40 +0000

    🔨 chore: add transations, scale to cards

--- src/modules/home/components/Card.component.tsx
+++ src/modules/home/components/Card.component.tsx
@@ -22,44 +22,46 @@ const CardComponent: React.FC<CardComponentProps> = ({
   const imgSrc = `${resizeImage}/${isImageError ? notFoundImage : image_url}`;
   const [isShowModal, setIsShowModal] = useState(false);
   return (
-    <div className="max-w-lg p-6 border rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <Image
-          className="rounded-t-lg"
-          src={imgSrc}
-          onError={() => setIsImageError(true)}
-          alt="Nft image"
-          height={200}
-          width={500}
-        />
-      </a>
-      <a href="#">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight overflow-hidden whitespace-nowrap text-ellipsis my-3 text-gray-900 dark:text-white">
-          {name}
-        </h5>
-      </a>
-      <p className="mb-3 font-normal overflow-hidden whitespace-nowrap text-ellipsis text-gray-700 dark:text-gray-400">
-        {description}
-      </p>
-      <button
-        onClick={() => setIsShowModal(true)}
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Read more
-        <svg
-          aria-hidden="true"
-          className="w-4 h-4 ml-2 -mr-1"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+    <>
+      <div className="max-w-lg p-6 border rounded-lg hover:border-green-500 hover:scale-105 transation duration-150 shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <Image
+            className="rounded-t-lg"
+            src={imgSrc}
+            onError={() => setIsImageError(true)}
+            alt="Nft image"
+            height={200}
+            width={500}
+          />
+        </a>
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight overflow-hidden whitespace-nowrap text-ellipsis my-3 text-gray-900 dark:text-white">
+            {name}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal overflow-hidden whitespace-nowrap text-ellipsis text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+        <button
+          onClick={() => setIsShowModal(true)}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </button>
+          Read more
+          <svg
+            aria-hidden="true"
+            className="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
       {isShowModal && (
         <ModalComponet
           setIsShow={setIsShowModal}
@@ -72,7 +74,7 @@ const CardComponent: React.FC<CardComponentProps> = ({
           assetContractAddress={asset_contract.address}
         />
       )}
-    </div>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,39 @@
 import CardComponent from "@/modules/home/components/Card.component";
 import SearchComponent from "@/modules/home/components/Search.component";
 import useGetAssets from "@/modules/home/hooks/useGetAssets";
+import Head from "next/head";
 import { useState } from "react";
 
 function Home() {
   const { assets, getAssets, isLoading, error } = useGetAssets();
   return (
-    <div className="md:flex flex-1 flex-col grid px-0 lg:px-60 py-4">
-      <SearchComponent getAssets={getAssets} />
-      {isLoading && (
-        <div className="flex h-screen justify-center items-center my-4">
-          <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-white" />
-        </div>
-      )}
-      {Boolean(error.message) && (
-        <div className="flex h-screen justify-center items-center my-4">
-          <h1 className="text-2xl font-bold text-white">{error.message}</h1>
-        </div>
-      )}
-      {!isLoading && !Boolean(error.message) && (
-        <div className="grid sm:grid-cols-2 gap-5 mt-5 ">
-          {assets.map((asset) => (
-            <div key={asset.id}>
-              <CardComponent {...asset} />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>SamuNFT</title>
+      </Head>
+      <div className="md:flex flex-1 flex-col grid px-0 lg:px-60 py-4">
+        <SearchComponent getAssets={getAssets} />
+        {isLoading && (
+          <div className="flex h-screen justify-center items-center my-4">
+            <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-white" />
+          </div>
+        )}
+        {Boolean(error.message) && (
+          <div className="flex h-screen justify-center items-center my-4">
+            <h1 className="text-2xl font-bold text-white">{error.message}</h1>
+          </div>
+        )}
+        {!isLoading && !Boolean(error.message) && (
+          <div className="grid sm:grid-cols-2 gap-5 mt-5 ">
+            {assets.map((asset) => (
+              <div key={asset.id}>
+                <CardComponent {...asset} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
